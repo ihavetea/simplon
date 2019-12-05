da4f22241bebe4bcdd6d194ffd6a69abcae64d74
ihavetea <ihavetea@gmail.com>
2019-12-05 18:03:59 +0100

    autopush

---
+++ Matthieu/assets/js/step3.js
@@ -0,0 +1,7 @@
+var william = document.querySelector('button');
+
+var elementHtml = document.getElementById('todolist');
+
+william.onclick = function (){
+  console.log("Faire du code");
+}
---
+++ Ryan/step3.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+var li = document.getElementById('todoList');
+var button = document.querySelector('button')
+var ajout = document.getElementsByClassName("button").innertext;
+
+button.onclick = function() {
+    console.log(ajout);
+};
+
+
+
+
