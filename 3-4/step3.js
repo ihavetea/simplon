372983b29726a33803e99dac765be0d4d4561fef
ihavetea <ihavetea@gmail.com>
2019-12-05 18:28:08 +0100

    autopush

--- 3-4/step3.js
+++ 3-4/step3.js
@@ -2,30 +2,30 @@
 //SALUT 
 
 // ICI LE JS SURVEILLE LE BOUTON HTML
-const button = document.querySelector('button');
+const surveilleBouton = document.querySelector('button');
 
 //ICI ON RECUPERE LE CONTENU du <ul id="todoList"></ul> ET ON LE STOCKE DANS UNE VARIABLE list
 //POUR EVITER DE DEVOIR RETAPER document.getElementById('todoList'), MAIS ON PEUT FAIRE SANS.
-let list = document.getElementById('todoList')
+let touteLaTodoList = document.getElementById('todoList')
 
 //ICI ON GENERE UN TAG <li></li> QUE L'ON VA UTILISER DANS LA FONCTION
-var newListItem = document.createElement("li");
+var tagsListeARajouter = document.createElement("li");
 
 //ICI ON GENERE LE TEXTE A METTRE DANS LE NOUVEAU <li></li> QUE L'ON VA UTILISER DANS LA FONCTION
-var addedText = document.createTextNode('Hello Brian');
+var texteARajouterDansLaListe = document.createTextNode('Hello Brian');
 
 
-// .onclick REAGIT a button ( = document.querySelector('button');) ET PERMET DE LANCER LA FONCTION
-button.onclick = function () {
+// .onclick REAGIT a surveilleBouton ( = document.querySelector('button');) ET PERMET DE LANCER LA FONCTION
+surveilleBouton.onclick = function () {
 
     //CONSOLE LOG POUR DEBUGGER C'EST OPTIONEL
-    console.log(list);
+    console.log(touteLaTodoList);
 
-    //ICI TU AJOUTES LE TEXTE Hello Brian STOCKE DANS LA VARIABLE newListItem DANS LES TAGS <li></li>
+    //ICI TU AJOUTES LE TEXTE Hello Brian STOCKE DANS LA VARIABLE TagsListeARajouter DANS LES TAGS <li></li>
     //APPEND VEUT DIRE RAJOUTER A LA FIN
-    newListItem.appendChild(addedText);
+    tagsListeARajouter.appendChild(texteARajouterDansLaListe);
 
     // ICI TU BALANCES TON COMBO <li>Hello Brian</li> EN BAS DU <ul id="todoList"> DANS LE HTML
-    // TU "APPEND" <li>Hello Brian</li> VERS document.getElementById('todoList') (= list)
-    list.appendChild(newListItem);
+    // TU "APPEND" <li>Hello Brian</li> VERS document.getElementById('todoList')
+    touteLaTodoList.appendChild(tagsListeARajouter);
 }
